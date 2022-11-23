40e0b66aac5cc2bda087876c1a144b566b6a39e3
ankit-dutta <ankitdutta965@gmail.com>
2022-11-24 01:13:27 +0530

    using key and adding items dynamically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,29 +5,29 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-const [title,setTitle] = useState(props.title);
-const [amount,setAmount]  = useState(props.amount)
+// const [title,setTitle] = useState(props.title);
+// const [amount,setAmount]  = useState(props.amount)
 
-const clickHandler = () =>{
-  setTitle('updated');
-  console.log(title);
-}
+// const clickHandler = () =>{
+//   setTitle('updated');
+//   console.log(title);
+// }
 
-const amountChange = () =>{
-  console.log("amount chaged")
-  let newAmount = '100'
-  setAmount(newAmount);
-}
+// const amountChange = () =>{
+//   console.log("amount chaged")
+//   let newAmount = '100'
+//   setAmount(newAmount);
+// }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>change title</button>
-      <button onClick = {amountChange}>change amount</button>
+      {/* <button onClick={clickHandler}>change title</button>
+      <button onClick = {amountChange}>change amount</button> */}
     </Card>
   );
 }
