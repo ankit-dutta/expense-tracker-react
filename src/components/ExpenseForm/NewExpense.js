4c354669e0c3e15a7442cd75f4f1239f73fc05f2
ankit-dutta <ankitdutta965@gmail.com>
2022-11-25 00:53:23 +0530

    adding add and cancel button

--- src/components/ExpenseForm/NewExpense.js
+++ src/components/ExpenseForm/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) =>{
+
+  const [firstAddBtn , setFirstAddBtn] = useState();
 
   const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
@@ -11,12 +13,21 @@ const NewExpense = (props) =>{
           id: Math.random().toString()
         }
         props.addExpense(expenseData)
+        setFirstAddBtn(false)
   }
 
+  const openAddBtnHandler = () =>{
+    setFirstAddBtn(true)
+  }
+   
+  const closeAddBtnHandler = () =>{
+    setFirstAddBtn(false)
+  }
     return(
         <>
           <div className="new-expense">
-            <ExpenseForm saveExpenseData ={saveExpenseDataHandler} />
+           {!firstAddBtn && <button onClick={openAddBtnHandler}>Add New Expense</button>}
+          {firstAddBtn && <ExpenseForm saveExpenseData ={saveExpenseDataHandler} onCancel = {closeAddBtnHandler} />}
 
           </div>
         </>
